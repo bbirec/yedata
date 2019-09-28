a84fb4f549c7526890fe8340422d3388dcd97e69
Heehong Moon(문희홍) <bbirec@gmail.com>
2019-09-28 15:23:47 +0700

    최신버전.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject yedata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Aurora Serverless Data API (feat. yesql)"
+  :url "https://github.com/bbirec/yedata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.6"]
                  [com.amazonaws/aws-java-sdk-rdsdata "1.11.638"]
-                 [instaparse "1.4.1" :exclusions [org.clojure/clojure]]])
+                 [instaparse "1.4.10" :exclusions [org.clojure/clojure]]])
